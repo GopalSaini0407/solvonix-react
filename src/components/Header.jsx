5bf1e79235cab8d24b83f922c7fd9b03f4b272a4
gopal <gopalsaini121001@gmail.com>
2025-04-04 00:43:32 +0530

    set proper header with 100vh in desktop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom';
 import Navbar from './Navbar';
 import HeaderBottom from './HeaderBottom';
+
 export default function Header() {
+  const location = useLocation();
+  const isHome = location.pathname === '/';
+
   return (
-    <header className='bg-[#01012B] pb-20'>
-        <div className='max-w-[1200px] mx-auto'>
+    <header className={`bg-[#01012B] pb-20 ${isHome ? 'home' : ''}`}>
+      <div className='max-w-[1200px] mx-auto'>
         <HeaderBottom/>
-        </div>
+      </div>
     </header>
   )
 }
+
